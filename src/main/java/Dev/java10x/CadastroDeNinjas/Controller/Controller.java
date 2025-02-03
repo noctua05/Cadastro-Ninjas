59ee978b4953ffa012c396964aa9499c029ac907
pedro <pedrocatarino246@gmail.com>
2025-02-02 23:04:31 -0300

    Primeiro Controler

---
+++ src/main/java/Dev/java10x/CadastroDeNinjas/Controller/Controller.java
@@ -0,0 +1,14 @@
+package Dev.java10x.CadastroDeNinjas.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasvindas")
+    public String boasVindas(){return "Essa é minha peimeira mensagem nessa rota";}
+
+}
