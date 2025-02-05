abd0140c7b16bc5034e7a8ac3b6a01d71cff3c29
pedro <pedrocatarino246@gmail.com>
2025-02-04 23:18:02 -0300

    feat: Implementa o repositorio de missao

---
+++ src/main/java/Dev/java10x/CadastroDeNinjas/Repository/MissoesRepository.java
@@ -0,0 +1,9 @@
+package Dev.java10x.CadastroDeNinjas.Repository;
+
+import Dev.java10x.CadastroDeNinjas.Model.MissoesModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MissoesRepository extends JpaRepository<MissoesModel,Long> {
+
+
+}
